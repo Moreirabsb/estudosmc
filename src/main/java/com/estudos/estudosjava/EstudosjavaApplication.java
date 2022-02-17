dee1498ad305a053666d5e5dd56c48c27477e415
Fernando de Paula <fernandodepaulabsb@gmail.com>
2022-02-17 09:43:26 -0300

    Projeto criado

---
+++ src/main/java/com/estudos/estudosjava/EstudosjavaApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.estudosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/estudosjava/EstudosjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.estudosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
